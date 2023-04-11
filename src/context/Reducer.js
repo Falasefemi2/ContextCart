4f8f066817e1f1610f7f0f7306a8756f90dc2ccf
Falase Femi <femifalase228@gmail.com>
2023-04-11 12:33:44 +0100

    more functionality

--- src/context/Reducer.js
+++ src/context/Reducer.js
@@ -1,19 +1,29 @@
 export const cartReducer = (state, action) => {
     switch (action.type) {
-      case "ADD_TO_CART":
-        return { ...state, cart: [...state.cart, { ...action.payload, qty: 1 }] };
-      case "REMOVE_FROM_CART":
+        case "ADD_TO_CART":
+      const existingItem = state.cart.find((item) => item.id === action.payload.id);
+      if (existingItem) {
         return {
           ...state,
-          cart: state.cart.filter((c) => c.id !== action.payload.id),
-        };
-      case "CHANGE_CART_QTY":
-        return {
-          ...state,
-          cart: state.cart.filter((c) =>
-            c.id === action.payload.id ? (c.qty = action.payload.qty) : c.qty
+          cart: state.cart.map((item) =>
+            item.id === action.payload.id ? { ...item, qty: item.qty + 1 } : item
           ),
         };
+      } else {
+        return { ...state, cart: [...state.cart, { ...action.payload, qty: 1 }] };
+      }
+      case "REMOVE_FROM_CART":
+      return {
+        ...state,
+        cart: state.cart.filter((c) => c.id !== action.payload.id),
+      };
+    case "CHANGE_CART_QTY":
+      return {
+        ...state,
+        cart: state.cart.map((c) =>
+          c.id === action.payload.id ? { ...c, qty: action.payload.qty } : c
+        ),
+      };
       default:
         return state;
     }
