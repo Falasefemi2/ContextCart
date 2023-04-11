7d49031c7a839e46722e728b7f01e7ec50e22757
Falase Femi <femifalase228@gmail.com>
2023-04-11 12:20:48 +0100

    implemented the context

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import { cartState } from "../context/Context";
+import Filters from "./Filters";
+import SingleProduct from "./SingleProducts";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const { state } = cartState();
+  const { products = [], productState = {} } = state;
+  const { sort, byStock, byFastDelivery, byRating, searchQuery } = productState;
 
-export default Home
+  const transformProducts = () => {
+    let sortedProducts = [...products];
+
+    if (sort) {
+      sortedProducts = sortedProducts.sort((a, b) =>
+        sort === "lowToHigh" ? a.price - b.price : b.price - a.price
+      );
+    }
+
+    if (!byStock) {
+      sortedProducts = sortedProducts.filter((prod) => prod.inStock);
+    }
+
+    if (byFastDelivery) {
+      sortedProducts = sortedProducts.filter((prod) => prod.fastDelivery);
+    }
+
+    if (byRating) {
+      sortedProducts = sortedProducts.filter(
+        (prod) => prod.ratings >= byRating
+      );
+    }
+
+    if (searchQuery) {
+      sortedProducts = sortedProducts.filter((prod) =>
+        prod.name.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+    }
+
+    return sortedProducts;
+  };
+
+  return (
+    <div className="home">
+      <Filters />
+      <div className="productContainer">
+        {transformProducts().map((prod) => (
+          <SingleProduct prod={prod} key={prod.i} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,21 +1,39 @@
-import React, { createContext } from "react";
+import React, { createContext, useContext, useReducer } from "react";
 import { faker } from "@faker-js/faker";
+import { cartReducer, productReducer } from "./Reducer";
+
 
 const cart = createContext();
+faker.seed(99);
 
 const context = ({ children }) => {
   const products = [...Array(20)].map(() => ({
-    id: faker.datatype.uuid(),
+    id: faker.datatype.uuid,
     name: faker.commerce.productName(),
     price: faker.commerce.price(),
-    image: faker.image.imageUrl(),
+    image: faker.image.image(),
     inStock: [0, 3, 5, 6, 7][Math.floor(Math.random() * 5)],
     fastDelivery: faker.datatype.boolean(),
     ratings: [1, 2, 3, 4, 5][Math.floor(Math.random() * 5)],
   }));
-  console.log(products);
 
-  return <cart.Provider>{children}</cart.Provider>;
+  const [state, dispatch] = useReducer(cartReducer, {
+    products: products,
+    cart: [],
+  });
+
+  const [productState, productDispatch] = useReducer(productReducer, {
+    byStock: false,
+    byFastDelivery: false,
+    byRating: 0,
+    searchQuery: "",
+  });
+
+  return <cart.Provider value={{ state, dispatch, productState, productDispatch }}>{children}</cart.Provider>;
 };
 
 export default context;
+
+export const cartState = () => {
+  return useContext(cart);
+}
